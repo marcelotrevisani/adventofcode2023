38ce435fc79c526847693c69e752bcc2ee33ed5d
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2023-12-02 11:01:35 +0000

    [Python] Day 2 - Solution 2

--- python/adoc23/day2.py
+++ python/adoc23/day2.py
@@ -15,3 +15,17 @@ def get_valid_game(line: str) -> int:
             if max_cubes[color] - int(val) < 0:
                 return 0
     return game
+
+
+def solution2(all_input: str) -> int:
+    return sum(get_power_for_game(line) for line in all_input.splitlines())
+
+
+def get_power_for_game(line: str) -> int:
+    re_set = re.compile(r"(\d+)\s+(red|blue|green)")
+    min_game = {}
+    for set_block in line.split(";"):
+        for val, color in re_set.findall(set_block):
+            val = int(val)
+            min_game[color] = max(val, min_game.get(color, val))
+    return min_game["red"] * min_game["blue"] * min_game["green"]
--- python/tests/test_day2.py
+++ python/tests/test_day2.py
@@ -1,4 +1,4 @@
-from adoc23.day2 import solution1
+from adoc23.day2 import solution1, solution2
 
 
 def test_solution1_simple():
@@ -13,3 +13,17 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
 def test_solution1(shared_datadir):
     all_input = (shared_datadir / "day2-solution1.txt").read_text()
     assert solution1(all_input) == 2685
+
+
+def test_solution2_simple():
+    all_input = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+    assert solution2(all_input) == 2286
+
+
+def test_solution2(shared_datadir):
+    all_input = (shared_datadir / "day2-solution2.txt").read_text()
+    assert solution2(all_input) == 83707
