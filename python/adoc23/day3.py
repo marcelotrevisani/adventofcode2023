d2de8714956bbe5076dc1693abedfef5fe57e6dd
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2023-12-04 07:07:08 +0000

    [Python] Day 3 - Solution 2

--- python/adoc23/day3.py
+++ python/adoc23/day3.py
@@ -1,3 +1,4 @@
+import re
 from typing import List
 
 
@@ -32,3 +33,28 @@ def is_part_number(row_pos: int, col_pos: int, matrix: List[List]) -> bool:
             if matrix[row][col] in special_char:
                 return True
     return False
+
+
+def solution2(all_input: str) -> int:
+    matrix = [list(line) for line in all_input.splitlines()]
+    result = 0
+    for row_pos, line in enumerate(matrix):
+        for col_pos, character in enumerate(line):
+            if character == "*":
+                result += get_gear_ratio(row_pos, col_pos, matrix)
+    return result
+
+
+def get_gear_ratio(row_pos: int, col_pos: int, matrix: List[List]) -> int:
+    star_range_col = {col_pos - 1, col_pos, col_pos + 1}
+    result = []
+    re_num = re.compile(r"(\d+)")
+    for row in range(row_pos - 1, row_pos + 2):
+        for found_num in re_num.finditer("".join(matrix[row])):
+            span_range = found_num.span()
+            span_range = {span_range[0], span_range[1] - 1}
+            if span_range.intersection(star_range_col):
+                result.append(int(found_num.group(1)))
+    if len(result) != 2:
+        return 0
+    return result[0] * result[1]
--- python/tests/test_day3.py
+++ python/tests/test_day3.py
@@ -1,4 +1,4 @@
-from adoc23.day3 import solution1
+from adoc23.day3 import solution1, solution2
 
 
 def test_solution1_simple():
@@ -17,4 +17,23 @@ def test_solution1_simple():
 
 def test_solution1(shared_datadir):
     all_input = (shared_datadir / "day3-input.txt").read_text()
-    assert solution1(all_input) == 0
+    assert solution1(all_input) == 538046
+
+
+def test_solution2_simple():
+    all_input = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."""
+    assert solution2(all_input) == 467835
+
+
+def test_solution2(shared_datadir):
+    all_input = (shared_datadir / "day3-input.txt").read_text()
+    assert solution2(all_input) == 81709807
