99823fd6fc7d12a4d3bc14f2b6865a5254ca3d0d
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2023-12-04 16:02:58 +0000

    [Python] Day 4 - Solution 2

--- python/adoc23/day4.py
+++ python/adoc23/day4.py
@@ -15,3 +15,27 @@ def get_points_by_card(line: str) -> int:
     if num_winning_numbers == 0:
         return 0
     return 2 ** (num_winning_numbers - 1)
+
+
+def get_num_winning_numbers(line: str) -> int:
+    stripped_line = line.split(":", 1)[1]
+    winning_numbers, my_card = stripped_line.split("|", 1)
+    re_numbers = re.compile(r"(\d+)")
+    winning_numbers = set(re_numbers.findall(winning_numbers))
+    my_card = set(re_numbers.findall(my_card))
+    return len(winning_numbers.intersection(my_card))
+
+
+def solution2(all_input: str) -> int:
+    list_lines = all_input.strip().splitlines()
+    all_winning_numbers = [get_num_winning_numbers(line) for line in list_lines]
+    num_cards = [1] * len(list_lines)
+    for pos, num_win in enumerate(all_winning_numbers):
+        if num_win == 0:
+            continue
+        for i in range(pos + 1, pos + num_win + 1):
+            if i >= len(list_lines):
+                num_cards[-1] += 1
+            else:
+                num_cards[i] += num_cards[pos]
+    return sum(num_cards)
