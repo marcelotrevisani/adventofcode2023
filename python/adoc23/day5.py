c491e9d91239e5143698d739da61f421de8a930c
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2023-12-07 08:30:01 +0000

    [Python] Day 5 - Solution 2

--- python/adoc23/day5.py
+++ python/adoc23/day5.py
@@ -1,11 +1,15 @@
+import dataclasses
+import multiprocessing
 import re
 from collections import namedtuple
 from typing import List
+import itertools
+
+SourceDest = namedtuple("SourceDest", ["source", "dest"])
 
 
 def build_converters(all_inputs: List[str]) -> List[List]:
     re_num = re.compile(r"(\d+)")
-    SourceDest = namedtuple("SourceDest", ["source", "dest"])
     result = []
     acc = []
     for line in all_inputs:
@@ -52,3 +56,45 @@ def solution1(all_input: str) -> int:
     all_converters = build_converters(all_lines[1:])
     all_seeds = [int(i.group(1)) for i in re_num.finditer(all_lines[0])]
     return min(get_final_number(seed, all_converters) for seed in all_seeds)
+
+
+def process_seed_chunk(seed_chunk, all_converters):
+    return min(get_final_number(seed, all_converters) for seed in seed_chunk)
+
+
+def solution2(all_input: str) -> int:
+    all_lines = all_input.splitlines()
+    all_seeds = []
+    for seed in re.finditer(r"(\d+)\s+(\d+)", all_lines[0]):
+        start, step = seed.groups()
+        start = int(start)
+        end = start + int(step)
+        all_seeds.append(range(start, end))
+    all_converters = build_converters(all_lines[1:])
+    num_processes = multiprocessing.cpu_count()
+    result = None
+    for batch in all_seeds:
+        if len(batch) <= num_processes:
+            seed_chunks = [batch]
+        else:
+            step = len(batch) // num_processes
+            start = batch.start
+            seed_chunks = [
+                range(start + (step * i), start + (step * (i + 1)))
+                for i in range(num_processes)
+            ]
+            seed_chunks[-1] = range(seed_chunks[-1].start, batch.stop)
+
+        with multiprocessing.Pool(processes=num_processes) as pool:
+            results = min(
+                pool.starmap(
+                    process_seed_chunk,
+                    [(chunk, all_converters) for chunk in seed_chunks],
+                )
+            )
+
+        if result is None:
+            result = results
+        else:
+            result = min(result, results)
+    return result
--- python/tests/test_day5.py
+++ python/tests/test_day5.py
@@ -1,4 +1,4 @@
-from adoc23.day5 import solution1
+from adoc23.day5 import solution1, solution2
 
 
 def test_solution1_simple():
@@ -41,3 +41,45 @@ humidity-to-location map:
 def test_solution1(shared_datadir):
     all_input = (shared_datadir / "day5.txt").read_text()
     assert solution1(all_input) == 26273516
+
+
+def test_solution2_simple():
+    all_input = """seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4"""
+    assert solution2(all_input) == 35
+
+
+def test_solution2(shared_datadir):
+    all_input = (shared_datadir / "day5.txt").read_text()
+    assert solution2(all_input) == 34039469
