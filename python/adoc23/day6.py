e370720fcd70c8deef6f691e621a0fea9293cc8b
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2023-12-08 18:57:34 +0000

    [Python] Day 6 - Solution 2

--- python/adoc23/day6.py
+++ python/adoc23/day6.py
@@ -4,7 +4,7 @@ from decimal import Decimal
 
 
 def calculate_num_possibilities(total_time: int, distance: int):
-    delta = Decimal((total_time ** 2) - (4 * distance)).sqrt()
+    delta = Decimal((total_time**2) - (4 * distance)).sqrt()
     root_left = max((-total_time + delta) / Decimal(-2), 0)
     if root_left.to_integral_value() == root_left:
         root_left += 1
@@ -15,7 +15,7 @@ def calculate_num_possibilities(total_time: int, distance: int):
         root_right -= 1
     else:
         root_right = math.floor(root_right)
-    return (root_right - root_left + 1)
+    return root_right - root_left + 1
 
 
 def solution1(all_input: str) -> int:
@@ -27,5 +27,18 @@ def solution1(all_input: str) -> int:
     num_runs = len(struct_input["Time"])
     result = 1
     for pos in range(num_runs):
-        result *= calculate_num_possibilities(struct_input["Time"][pos], distance=struct_input["Distance"][pos])
+        result *= calculate_num_possibilities(
+            struct_input["Time"][pos], distance=struct_input["Distance"][pos]
+        )
     return result
+
+
+def solution2(all_input: str) -> int:
+    re_num = re.compile(r"(\d+)")
+    struct_input = {}
+    for line in all_input.splitlines():
+        key, values = line.strip().split(":")
+        struct_input[key] = int("".join(re_num.findall(values)))
+    return calculate_num_possibilities(
+        struct_input["Time"], distance=struct_input["Distance"]
+    )
--- python/tests/test_day6.py
+++ python/tests/test_day6.py
@@ -1,4 +1,4 @@
-from adoc23.day6 import solution1
+from adoc23.day6 import solution1, solution2
 
 
 def test_solution1_simple():
@@ -11,3 +11,13 @@ def test_solution1(shared_datadir):
     all_input = (shared_datadir / "day6.txt").read_text()
     assert solution1(all_input) == 0
 
+
+def test_solution2():
+    all_input = """Time:      7  15   30
+    Distance:  9  40  200"""
+    assert solution2(all_input) == 71503
+
+
+def test_solution2(shared_datadir):
+    all_input = (shared_datadir / "day6.txt").read_text()
+    assert solution2(all_input) == 0
