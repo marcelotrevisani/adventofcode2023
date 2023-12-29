9cddb9e33150b142e4cc2733d13ab4d5d882255c
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2023-12-29 10:58:09 +0000

    [Python] Day 11 - Solution 2

--- python/adoc23/day11.py
+++ python/adoc23/day11.py
@@ -39,3 +39,35 @@ def get_all_points(all_input: list[list[str]]) -> list[tuple[int, int]]:
             if col != ".":
                 all_points.append((line_pos, col_pos))
     return all_points
+
+
+def get_all_points_without_changing_universe(
+    all_input: list[list[str]], expansion: int
+) -> list[tuple[int, int]]:
+    blank_col, blank_line = get_expanded_positions(all_input)
+    blank_col.sort()
+    blank_line.sort()
+    result = []
+    for line_pos, line in enumerate(all_input):
+        for col_pos, col in enumerate(line):
+            if col == ".":
+                continue
+            num_line_shift = sum(line_pos > val for val in blank_line)
+            shift_line = num_line_shift * expansion
+            num_col_shift = sum(col_pos > val for val in blank_col)
+            shift_col = num_col_shift * expansion
+            if shift_line > 0:
+                shift_line -= num_line_shift
+            if shift_col > 0:
+                shift_col -= num_col_shift
+            result.append((line_pos + shift_line, col_pos + shift_col))
+    return result
+
+
+def solution2(all_input: str, expansion: int = 1_000_000) -> int:
+    all_input = [list(line) for line in all_input.splitlines()]
+    all_points = get_all_points_without_changing_universe(all_input, expansion)
+    result = 0
+    for point1, point2 in itertools.combinations(all_points, 2):
+        result += abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+    return result
--- python/tests/test_day11.py
+++ python/tests/test_day11.py
@@ -1,4 +1,6 @@
-from adoc23.day11 import solution1
+import pytest
+
+from adoc23.day11 import solution1, solution2
 
 
 def test_solution1_simple():
@@ -18,3 +20,23 @@ def test_solution1_simple():
 def test_solution1(shared_datadir):
     all_input = (shared_datadir / "day11.txt").read_text()
     assert solution1(all_input) == 9639160
+
+
+@pytest.mark.parametrize("expansion, result", [(10, 1030), (100, 8410)])
+def test_solution2_simple(expansion, result):
+    all_input = """...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."""
+    assert solution2(all_input, expansion) == result
+
+
+def test_solution2(shared_datadir):
+    all_input = (shared_datadir / "day11.txt").read_text()
+    assert solution2(all_input) == 752936133304
